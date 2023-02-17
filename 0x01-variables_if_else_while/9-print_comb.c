11e655507a5e7bd828c2c84d9c976c379291a49f
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-02-18 00:48:53 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,27 @@
 /*
- * File: 5-print_numbers.c
+ * File: 9-print_comb.c
  * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0.
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-
-	printf("\n");
-
+	putchar('\n');
 	return (0);
 }
