ed541cc6f2e93b909c55cf617fbfc24aa2759dfb
Super User <haidyasser3@gmail.com>
2023-02-22 10:57:23 -0800

    printing letters 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ *Return : 0
+ */
+void print_alphabet_x10(void)
+{
+	int cnt = 1;
+
+	while (cnt <= 10)
+	{
+		print_alphabet();
+	}
+}
