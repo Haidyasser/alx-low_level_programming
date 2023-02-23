4df1cde03d49c501ef210a5a9d3f2997aaed250d
Super User <haidyasser3@gmail.com>
2023-02-23 12:47:22 -0800

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,8 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n / 10 + '0');
+			if (n > 9)
+				_putchar(n / 10 + '0');
 			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
