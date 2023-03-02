9e20fc629eb084d8d82356e4a8755f067bbd6d4c
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-03-02 15:02:59 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int index = 0, src_len = 0;
 
-	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[i];
-	if (!src[i])
-		dest[i] = src[i];
-	return (dest);
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 }
