0aa5219ae71b3d6b555167201c0ea51615eba48a
Super User <haidyasser3@gmail.com>
2023-03-03 03:33:23 -0800

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int i = 0;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+	if (!src[i])
+		while(dest[i])
+			dest[i++] = '\0';
+	return (dest);
 }
