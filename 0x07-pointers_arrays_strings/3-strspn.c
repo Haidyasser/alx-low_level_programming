9b2075b9c6523c162f2eb5c31780eb32ed9506ca
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-03-06 19:30:19 +0200

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,13 +10,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	int vis[30] = {0};
+	unsigned int bytes = 0;
+	int index;
 
-	while (s[i])
-		vis[s[i++] - 'a']++;
-	i = 0;
-	while (accept[i] && vis[accept[i] - 'a'])
-		i++;
-	return (i);
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
 }
