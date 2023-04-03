adcfcd92556e4c3a7bab869cf055b01cbd7b8426
Haidyasser <haidyasser3@gmaail.com>
2023-04-03 20:44:05 +0200

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,6 +15,17 @@ void _puts(char *str)
 }
 
 /**
+ * print_unsigned_int - prints an unsigned int to stdout
+ * @n: The unsigned int to print
+ */
+void print_unsigned_int(unsigned int n)
+{
+	if (n / 10)
+		print_unsigned_int(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
  * Return: the number of nodes
@@ -34,7 +45,7 @@ size_t print_list(const list_t *h)
 		{
 		_putchar('[');
 		/* print the length of the string here */
-		_putchar(h -> len  + '0');
+		print_unsigned_int(h -> len);
 		_putchar(']');
 		_putchar(' ');
 		_puts(h->str);
