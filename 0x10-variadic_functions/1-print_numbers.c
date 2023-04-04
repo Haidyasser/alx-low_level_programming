481b98f52506eb8150bb61aadb471a8e38a0eaa8
Haidyasser <haidyasser3@gmaail.com>
2023-04-05 01:44:15 +0200

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,9 +4,10 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int;
 
 	va_start(args, n);
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i < n - 1)
