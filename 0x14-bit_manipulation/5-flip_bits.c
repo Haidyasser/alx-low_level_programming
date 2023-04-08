94e00b8c76f1b00ea2a498cd04c704423fc97b8f
Haidyasser <haidyasser3@gmaail.com>
2023-04-08 23:56:29 +0200

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,12 +12,13 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int cnt = 0, xor = n ^ m;
+	unsigned int cnt = 0;
+	unsigned long int xor = (1UL * n) ^ m;
 
 	while (xor)
 	{
 		cnt += xor & 1;
-		xor >>= 1;
+		xor >>= 1UL;
 	}
 	return (cnt);
 }
