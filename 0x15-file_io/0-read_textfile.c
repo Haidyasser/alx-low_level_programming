080b0dc2025869964ba701f3b4214f23333e97e5
Haidyasser <haidyasser3@gmaail.com>
2023-04-10 17:26:42 +0200

    print file content

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,30 +2,37 @@
 #include<fcntl.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters read and printed, or 0 if an error occurs
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, n_read, n_write;
+	int fd
+	ssize_t = n_read, n_write;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
 	buf = malloc(letters);
-
 	if (!buf)
 		return (0);
 
 	n_read = read(fd, buf, letters);
-
 	if (n_read == -1)
 		return (0);
+
 	n_write = write(1, buf, n_read);
-	if (n_write == -1)
+	if (n_write == -1 || n_write != letters)
 		return (0);
 
 	free(buf);
