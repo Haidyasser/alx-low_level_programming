672edfd97e9027909863867a041db3853b606f50
Haidyasser <haidyasser3@gmaail.com>
2023-04-10 17:22:13 +0200

    print file content

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n_read, n_write;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+
+	if (!buf)
+		return (0);
+
+	n_read = read(fd, buf, letters);
+
+	if (n_read == -1)
+		return (0);
+	n_write = write(1, buf, n_read);
+	if (n_write == -1)
+		return (0);
+
+	free(buf);
+	close(fd);
+
+	return (n_write);
+}
