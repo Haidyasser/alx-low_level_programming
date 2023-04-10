c0b0f88c5f73e9a636a827f483bd9d0c1486c1bd
Haidyasser <haidyasser3@gmaail.com>
2023-04-10 21:21:40 +0200

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (~fd)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd != -1)
 		return (-1);
 	if (text_content)
 		n = write(fd, text_content, strlen(text_content));
 	close(fd);
-	return (~n ? 1 : -1);
+	return (n != -1 ? 1 : -1);
 }
